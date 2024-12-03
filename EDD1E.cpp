0ec8cd2ace2a01e8edec61963c51673d739c43bd
Milk <soulsinc115@gmail.com>
2024-12-03 00:37:51 -0500

    Update EDD1E.cpp

--- EDD1E.cpp
+++ EDD1E.cpp
@@ -1,10 +1,10 @@
-
 #include <iostream>
 #include <conio.h>
 #include <cstdlib> //<-- for RNG?//
 #include <thread> // for sleep //
 #include <chrono> // for timing//
 #include "GameBoard.h"
+#include "SnakeBody.h"
 
 using std::cout;
 using std::endl;
@@ -17,12 +17,11 @@ int main() {
 
 	int fruitX = rand() % 20;//food spawn
 	int fruitY = rand() % 20;
-	int x = 10, y = 10; //self spawn point//
-
-
-	GameBoard gameBoard(20, 20);
-
-
+	
+	SnakeBody snake(10, 10); // head spawn
+	
+	GameBoard gameBoard(20, 20); // board spawn
+	
 	char input;
 
 
@@ -31,7 +30,8 @@ int main() {
 		//^---this gave me an issue as i removed the "{" making an infinite loop// changed from system("cls") too in class operation 
 
 		gameBoard.setPosition(fruitX, fruitY, '*'); //food
-		gameBoard.setPosition(x, y, '0'); //Snake head lol
+		auto head = snake.getHead();
+		gameBoard.setPosition(head.first, head.second, '0'); //Snake head lol
 
 		gameBoard.display();
 	
@@ -39,16 +39,17 @@ int main() {
 		if (_kbhit()) {
 			input = _getch(); //reads keystrokes//
 			switch (input) {
-			case 'w': y = max(0, y - 1); break;
-			case 's': y = min(19, y + 1); break;
-			case 'a': x = max(0, x - 1); break;
-			case 'd': x = min(19, x + 1); break;
+			case 'w': snake.move(0, -1); break;
+			case 's': snake.move(0, 1); break;
+			case 'a': snake.move(-1, 0); break;
+			case 'd': snake.move(1, 0); break;
 			case 'x': return 0;
 				//classic "w,a,s,d" movment "x" to exit game//
 			}
 		}
 		//snake eat fruit check//
-		if (x == fruitX && y == fruitY) {
+		if (head.first == fruitX && head.second == fruitY) {
+			snake.grow();
 			fruitX = rand() % 20;//spawns new food.//
 			fruitY = rand() % 20;
 			gameBoard.incrementScore(); //++ score
